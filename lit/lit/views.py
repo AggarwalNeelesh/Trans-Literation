0c15ce224128825aebbdad1843c311d099641db9
Neelesh Aggarwal <86107529+AggarwalNeelesh@users.noreply.github.com>
2022-07-14 01:13:45 +0530

    Third commit

--- lit/lit/views.py
+++ lit/lit/views.py
@@ -5,12 +5,17 @@ from googletrans import Translator
 
 
 def index(request):
+    # Method to telecast the main page
+
     return render(request, 'index.html')
 
 
 def transliteration(request):
+    # Method for translating any language into English
+
     msg = request.POST.get('msg')
     translator = Translator()
     data = translator.translate(msg).text
     response = json.dumps(data, default='str')
+
     return HttpResponse(response)
